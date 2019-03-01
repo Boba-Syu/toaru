567e27aa724a6052bd925d0f580ace7040a42a6b
Boba-Syu <1914808114@qq.com>
2019-03-01 18:04:01 +0800

    asd

--- src/main/java/com/hunau/controller/UserController.java
+++ src/main/java/com/hunau/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+
 /**
  * Created by MI on 2019/2/7.
  */
@@ -17,29 +19,40 @@ public class UserController {
     UserService userService;
 
     @RequestMapping("userLogin")
-    public String userLogin(@RequestParam(value = "ObjectUser") String user) {
-        JSONObject j = JSONObject.fromObject(user);
-        User user1 = (User) JSONObject.toBean(j, User.class);
-        User user2 = userService.login(user1.getName());
-        if( user1.getPwd().equals(user2.getPwd()) ) {
-            return "success";
-        } else {
-            return "defeat";
-        }
+    public Callable<String> userLogin(@RequestParam(value = "ObjectUser") String user) {
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                JSONObject j = JSONObject.fromObject(user);
+                User user1 = (User) JSONObject.toBean(j, User.class);
+                User user2 = userService.login(user1.getName());
+                if( user1.getPwd().equals(user2.getPwd()) ) {
+                    return "success";
+                } else {
+                    return "defeat";
+                }
+            }
+        };
+        return callable;
+
     }
 
     @RequestMapping("userRegister")
-    public String userRegister(@RequestParam(value = "ObjectUser") String user) {
-        JSONObject j = JSONObject.fromObject(user);
-        User user1 = (User) JSONObject.toBean(j, User.class);
-        int result = userService.register(user1);
-/*        System.out.println(user);
-        System.out.println(user1);*/
-        if(result == 1) {
-            return "success";
-        } else {
-            return "defeat";
-        }
+    public Callable<String> userRegister(@RequestParam(value = "ObjectUser") String user) {
+        Callable<String> callable = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                JSONObject j = JSONObject.fromObject(user);
+                User user1 = (User) JSONObject.toBean(j, User.class);
+                int result = userService.register(user1);
+                if(result == 1) {
+                    return "success";
+                } else {
+                    return "defeat";
+                }
+            }
+        };
+        return callable;
     }
 
 }
