4d41d8a4d448fc05205626fffba9fbff41fb76af
Boba-Syu <1914808114@qq.com>
2019-03-04 15:36:21 +0800

    asd

--- src/main/resources/mysql_backcup/dbuser_stu_message.sql
+++ src/main/resources/mysql_backcup/dbuser_stu_message.sql
@@ -16,8 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping routines for database 'dbuser'
+-- Table structure for table `stu_message`
 --
+
+DROP TABLE IF EXISTS `stu_message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `stu_message` (
+  `name` varchar(45) NOT NULL DEFAULT '',
+  `text` varchar(255) NOT NULL,
+  `time` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`text`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stu_message`
+--
+
+LOCK TABLES `stu_message` WRITE;
+/*!40000 ALTER TABLE `stu_message` DISABLE KEYS */;
+INSERT INTO `stu_message` VALUES ('都市传说','一方通行的败北! 学园都市最强超能力者竟被无能力者打败!','2019-03-04 14:57:47'),('都市传说','学园都市天气预报频频出错! 相传树状图设计者以外被毁?','2019-03-04 15:06:19'),('都市传说','超能力者的噩梦! 第七学院出现能消除他人能力的学生?','2019-03-04 14:56:15'),('都市传说','震惊! 学园都市曝光正在进行克隆项目, 竟是为了量产超能力者???','2019-03-04 14:52:45');
+/*!40000 ALTER TABLE `stu_message` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -28,4 +49,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-25 17:19:22
+-- Dump completed on 2019-03-04 15:35:03
